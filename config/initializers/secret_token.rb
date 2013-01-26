802d00f0a05883a663a9ea8ad0e99564720ec37e
Nunzio Fiore <tempozero@gmail.com>
2013-01-26 16:19:33 +0100

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blio::Application.config.secret_token = '48e678bd3152984a331800a3ed4b717c6915da8ccd181cbbecd8c44555da003b2d08e8574549f18348861147cefe832b3950fe060f466e8f2239ac03fce8828a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Blio::Application.config.session_store :cookie_store, key: '_blio_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Blio::Application.config.session_store :active_record_store
